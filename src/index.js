c44fbc422344efbb33840f085b8c47e5f7527c4b
RexterR <sumandas.workplace@gmail.com>
2020-06-02 10:50:18 +0530

    first commit f

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const server = new ApolloServer({
+  typeDefs
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`)
+});
